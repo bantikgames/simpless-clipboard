80e7b62c0b9bed801e386f3d002fea9107a2cb00
bantikgames <anishenko.alex@gmail.com>
2022-07-22 18:33:04 +0500

    add some features

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import keyboard
+import pyperclip
 import sys
+import time
+
+from PyQt6.QtGui import QIcon
 from PyQt6.QtWidgets import (
     QApplication,
     QSystemTrayIcon,
@@ -8,51 +13,12 @@ from PyQt6.QtWidgets import (
     QVBoxLayout,
     QWidget,
     QPushButton)
-from PyQt6.QtGui import QIcon, QClipboard
-from pynput import keyboard
 
-# Создаем основное приложение
-
+# Создаем основное приложение и доступ к буферу обмена
 app = QApplication(sys.argv)
 app.setQuitOnLastWindowClosed(False)
 
-clipboard = QApplication.clipboard()
-
-# Комбинации горячих клавиш
-COPY_COMBINATIONS = [
-    {keyboard.Key.ctrl_l, keyboard.KeyCode(char='c')},
-    {keyboard.Key.ctrl_r, keyboard.KeyCode(char='c')}
-]
-
-# Текущие нажатые клавиши
-current_copy = set()
-
-
-class ListWidget(QListWidget):
-    def __init__(self):
-        super(ListWidget, self).__init__()
-        self.setUniformItemSizes(True)
-
-    def mouseDoubleClickEvent(self, event):
-        show_tray_message(trayIcon, "Текст скопирован в буфер обмена", self.currentItem().text())
-        clipboard.setText(self.currentItem().text())
-
-
-widget_list = ListWidget()
-
-
-def click_on_button():
-    with open("clipboard_history.txt", "r+") as file:
-        file.truncate(0)
-    widget_list.clear()
-
-
-def copy_text():
-    current_clipboard_text = clipboard.text(mode=QClipboard.Mode.Selection)
-    if current_clipboard_text and not current_clipboard_text.isspace():
-        with open("clipboard_history.txt", "r+") as file:
-            file.write(current_clipboard_text.strip() + '\n')
-            widget_list.addItem(current_clipboard_text.strip())
+clipboard = pyperclip
 
 
 class MainWindow(QMainWindow):
@@ -62,7 +28,7 @@ class MainWindow(QMainWindow):
         self.setWindowIcon(QIcon('simpless_icon64.png'))
 
         btn_clear = QPushButton("Очистить историю буфера обмена")
-        btn_clear.pressed.connect(click_on_button)
+        btn_clear.pressed.connect(self.clean_clipboard_history)
 
         layout = QVBoxLayout()
         layout.addWidget(widget_list)
@@ -72,9 +38,23 @@ class MainWindow(QMainWindow):
         widget.setLayout(layout)
         self.setCentralWidget(widget)
 
-        clipboard.dataChanged.connect(copy_text)
+    def clean_clipboard_history(self):
+        with open("clipboard_history.txt", "r+") as file:
+            file.truncate(0)
+        widget_list.clear()
 
 
+class ListWidget(QListWidget):
+    def __init__(self):
+        super(ListWidget, self).__init__()
+        self.setUniformItemSizes(True)
+
+    def mouseDoubleClickEvent(self, event):
+        show_tray_message(trayIcon, "Текст скопирован в буфер обмена", self.currentItem().text())
+        clipboard.copy(self.currentItem().text())
+
+
+widget_list = ListWidget()
 main_window = MainWindow()
 
 # Создаем меню для основного приложения и виджета
@@ -105,23 +85,17 @@ def load_clipboard_history():
             widget_list.addItem(i.strip())
 
 
-# Обрабатываем нажатие и отпускание клавиш копирования и вставки
-def on_press(key):
-    if any([key in comb for comb in COPY_COMBINATIONS]):
-        current_copy.add(key)
-        if any(all(k in current_copy for k in comb) for comb in COPY_COMBINATIONS):
-            copy_text()
-
-
-def on_release(key):
-    try:
-        current_copy.remove(key)
-    except KeyError:
-        pass
+def keyboard_pressed(event):
+    if keyboard.is_pressed('ctrl'):
+        time.sleep(0.2)
+        current_clipboard_text = clipboard.paste()
+        if current_clipboard_text and not current_clipboard_text.isspace():
+            with open("clipboard_history.txt", "a") as file:
+                file.write(current_clipboard_text.strip() + '\n')
+                widget_list.addItem(current_clipboard_text.strip())
 
 
 if __name__ == '__main__':
+    keyboard.on_press_key('c', keyboard_pressed)
     load_clipboard_history()
-    keyboard_listener = keyboard.Listener(on_press=on_press, on_release=on_release)
-    keyboard_listener.start()
     sys.exit(app.exec())
